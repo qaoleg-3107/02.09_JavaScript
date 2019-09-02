a9a8ee9f67bea4813b3658d77a00fec3febb0c32
qaoleg-3107 <qaoleg31@gmail.com>
2019-09-02 16:06:53 +0000

    1

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//for (let i = 0; i < 100; i= i + 1) {
+//document.write(i + '<br>');
+//}
+
+//for (let i = 100; i >= 0; i= i -1) {
+//document.write(i + '<br>');
+//}
+
+//for (let i = 100; i >= 0; i= i -2) {
+//document.write(i + '<br>');
+//}
+
+let products = ['a', 'b', 'c'];
+products[3] = 'd';
+products.push('zaz_e', 'f');
+console.log(products.length);
+
+
+
+
