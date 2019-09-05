83179634bd8278a5218cd2e46664b5867f809a6d
qaoleg-3107 <qaoleg31@gmail.com>
2019-09-05 16:14:22 +0000

    2

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
-//for (let i = 0; i < 100; i= i + 1) {
-//document.write(i + '<br>');
-//}
+// for (let i = 0; i <= 100; i= i + 1) {
+// document.write(i + '<br>');
+// }
 
-//for (let i = 100; i >= 0; i= i -1) {
-//document.write(i + '<br>');
-//}
+// for (let i = 100; i >=0 ; i--) {
+//  if (i % 2 !== 0 && i % 11 !== 0)
+//  document.write(i + '<br>');
+// }
 
-//for (let i = 100; i >= 0; i= i -2) {
-//document.write(i + '<br>');
-//}
+// let i = 0;
+// while (i <= 100 ) {
+//     document.write(i + '<br>');
+//     i++;
+// }
 
-let products = ['a', 'b', 'c'];
-products[3] = 'd';
-products.push('zaz_e', 'f');
-console.log(products.length);
+// let i = 0;
+// do {
+//    document.write(i + '<br>');
+//     i++;
+// } while (i < 100 );
 
+//  let num = parseFloat(prompt('Enter number'));
+//  while (isNaN(num))  {
+//      alert('You enterd is not a number!');
+//      num = parseFloat(prompt('Enter number'));
+//  }
 
-
-
+let num;
+do {
+    if (num !== undefined) {
+alert('You enterd is not a number!');
+}
+    num = parseFloat(prompt('Enter number'));
+}   while (isNaN(num))
