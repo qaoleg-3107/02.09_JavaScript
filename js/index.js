aebcfdd5eac29172c4b93321b55093e7b8766612
qaoleg-3107 <qaoleg31@gmail.com>
2019-09-06 00:10:47 +0000

    3

--- js/index.js
+++ js/index.js
@@ -25,10 +25,28 @@
 //      num = parseFloat(prompt('Enter number'));
 //  }
 
-let num;
-do {
-    if (num !== undefined) {
-alert('You enterd is not a number!');
-}
-    num = parseFloat(prompt('Enter number'));
-}   while (isNaN(num))
+// let num;
+// do {
+//     if (num !== undefined) {
+// alert('You enterd is not a number!');
+// }
+//     num = parseFloat(prompt('Enter number'));
+// }   while (isNaN(num))
+
+let products = ['Tesla X', 'Tesla S', 'BMW i3'];
+products[3] = 'Tesla Y';
+products.push ('ZAZ Sens', 'Chevy Corvette');
+products[10] = 'Toyota';
+
+// console.log(products.length);
+
+// for (let i = 0; i < products.length; i++) {
+//     if (products[i] !== undefined) {  // почему еще одни {} - как в матрёшке, не могу понять правила их расстановки; и почему нельзя использовать ===
+//     console.log(products[i]);
+//     }
+// }
+
+products.forEach(function(item) {
+    console.log(item);
+})
+
